2a486310e81382ead51652f8aec194f06afbca85
SaiDatta123 <saidatta111@gmail.com>
2024-03-18 20:05:12 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/wipro/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/wipro/exceptions/ResourceNotFoundException.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-@Setter
-@Getter
 public class ResourceNotFoundException extends RuntimeException{
 
     private String message;
